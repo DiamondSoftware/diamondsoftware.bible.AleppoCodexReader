ff158e1fa9a42d5d88133604c078803acb15f8e4
Mark <maracx@gmail.com>
2013-06-25 22:20:36 +0100

    Animation and transition

--- src/com/example/diamondsoftware/bible/aleppocodexreader/AleppoSplashActivity.java
+++ src/com/example/diamondsoftware/bible/aleppocodexreader/AleppoSplashActivity.java
@@ -4,8 +4,13 @@ package com.example.diamondsoftware.bible.aleppocodexreader;
  * maracx@gmail.com
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class AleppoSplashActivity extends AleppoActivity {
 
@@ -13,6 +18,29 @@ public class AleppoSplashActivity extends AleppoActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_aleppo_splash);
+		
+		TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+		Animation fade1 = AnimationUtils.loadAnimation(this,  R.anim.fade_in);
+		logo1.startAnimation(fade1);
+		fade1.setAnimationListener(new AnimationListener()
+	    {   public void onAnimationEnd(Animation animation) 
+	        {   
+	            Intent intent = new Intent(AleppoSplashActivity.this, AleppoMainMenuActivity.class);
+	            startActivity(intent);      
+	            finish();
+	        }
+	
+		@Override
+		public void onAnimationRepeat(Animation arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onAnimationStart(Animation arg0) {
+			// TODO Auto-generated method stub
+			
+		}});
 	}
 
 	@Override
@@ -22,4 +50,11 @@ public class AleppoSplashActivity extends AleppoActivity {
 		return true;
 	}
 
+	@Override
+	public void onPause() {
+		super.onPause();
+		TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+		logo1.clearAnimation();
+	}
+	
 }
